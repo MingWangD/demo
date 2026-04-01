863d72c53faaad0e8a2ffdbbd6913489dc27fa21
code2026 <wmywqd@gmail.com>
2026-04-01 20:34:25 +0800

    second

--- springboot/src/main/java/com/example/service/TeacherService.java
+++ springboot/src/main/java/com/example/service/TeacherService.java
@@ -19,6 +19,7 @@ public class TeacherService {
     @Resource private HomeworkMapper homeworkMapper;
     @Resource private HomeworkSubmissionMapper homeworkSubmissionMapper;
     @Resource private ExamRecordMapper examRecordMapper;
+    @Resource private ExamMapper examMapper;
     @Resource private ExamService examService;
 
     public List<Course> courseList(Long teacherId) {
